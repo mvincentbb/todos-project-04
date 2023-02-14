1d39692d3186962ac78f4c80462e74381cd93707
vincent <vincent.bakpatina@ipnetinstitute.com>
2023-02-14 23:12:21 +0000

    get todos features

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -10,7 +10,6 @@ export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
     // TODO: Implement creating a new TODO item
-
       const userId = getUserId(event)
       const newItem = await createTodo(newTodo,userId)
       return {
